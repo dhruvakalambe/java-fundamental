90e05d04887fc7429d9ed49344df77e2689d4200
dhruvakalambe <dhruvakalambe1785@gmail.com>
2026-03-06 23:23:37 +0530

    Loops implementation tryed

--- src/basic/loops/dowoile.java
+++ src/basic/loops/dowoile.java
@@ -1,2 +1,12 @@
-package basic.loops;public class dowoile {
+package basic.loops;
+
+public class dowoile {
+    public static void main(String[] args) {
+        int a=1;
+        do {
+            System.out.println("The value of a is "+ a );
+            a++;
+        }
+        while (a<=10);
+    }
 }
--- src/basic/loops/woile.java
+++ src/basic/loops/woile.java
@@ -1,2 +1,12 @@
-package basic.loops;public class woile {
+package basic.loops;
+
+public class woile {
+    public static void main(String[] args) {
+        int a=0;
+        System.out.println("The value of a should increase till 10");
+        while (a<=10){
+            System.out.println(a);
+            a++;
+        }
+    }
 }
