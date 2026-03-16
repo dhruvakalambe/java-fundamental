47f2c12d6392addb5cd6ca36a04baf7b69833032
dhruvakalambe <dhruvakalambe1785@gmail.com>
2026-03-16 23:47:56 +0530

    Arrray tryed

--- src/basic/methods/addition.java
+++ src/basic/methods/addition.java
@@ -1,2 +1,22 @@
-package basic.methods;public class addition {
+package basic.methods;
+import java.util.*;
+
+public class addition {
+    float add(float a, float b){
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number decimals are also acceptable 👍:\n");
+        float a = sc.nextFloat();
+        System.out.println("Enter another number:\n");
+        float b = sc.nextFloat();
+        System.out.println("Sum using function");
+        //creating an object of the class to call the 'add' method
+        addition obj = new addition();
+        //store the returned value in a variable and call the method via object
+        float sum = obj.add(a,b);
+        System.out.println("The sum of these 2 numbers is -\n"+a+"+"+b+"="+sum);
+    }
 }
--- src/basic/methods/prent.java
+++ src/basic/methods/prent.java
@@ -1,2 +1,16 @@
-package basic.methods;public class prent {
+package basic.methods;
+import java.util.*;
+
+public class prent {
+    static void shout(String s){
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any line:\n");
+        String s = sc.nextLine();
+        System.out.println("Line will be repeated using a function :)");
+        shout(s);
+    }
 }
